7835a2966d06c4a7a9205f88fcbff2422ed6414b
Tom Kraljevic <tomk@tomk.net>
2016-12-02 18:55:56 -0800

    Added Gradient Boosting Machine.

--- h2o-genmodel/src/main/cxx/h2o/ModelMojoFactory.h
+++ h2o-genmodel/src/main/cxx/h2o/ModelMojoFactory.h
@@ -2,12 +2,17 @@
 #define H2O_MODELMOJOFACTORY_H 1
 
 #include "h2o/ModelMojoReader.h"
+#include "h2o/GbmMojoReader.h"
 #include <cassert>
 
 namespace h2o {
 
 ModelMojoReader getMojoReader(const std::string &algo) {
-        assert(0);
+    if (algo == "Gradient Boosting Machine") {
+        return GbmMojoReader();
+    }
+
+    throw std::invalid_argument("Unsupported MOJO algorithm: " + algo);
 }
 
 }
