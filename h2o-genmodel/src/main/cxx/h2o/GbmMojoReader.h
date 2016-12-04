de30d7ae4ab37b2d2e14d2da88619f77cc4c4e16
Tom Kraljevic <tomk@tomk.net>
2016-12-03 16:04:49 -0800

    Add stub for GbmMojoReader.

---
+++ h2o-genmodel/src/main/cxx/h2o/GbmMojoReader.h
@@ -0,0 +1,14 @@
+#ifndef H2O_GBMMOJOREADER_H
+#define H2O_GBMMOJOREADER_H 1
+
+#include "h2o/ModelMojoReader.h"
+
+namespace h2o {
+
+class GbmMojoReader : public ModelMojoReader {
+
+};
+
+}
+
+#endif
